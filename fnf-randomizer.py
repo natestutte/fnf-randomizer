8f923dacb5dbb7be34c824126cc865daa0556790
Nate Stutte <ngs33@drexel.edu>
2021-01-23 14:01:20 -0500

    Added ability to randomize characters and sections

--- fnf-randomizer.py
+++ fnf-randomizer.py
@@ -5,7 +5,24 @@ import string
 import random
 import os
 
-def randomize(song):
+def randomize(song, character, order):
+
+    # gf-christmas crashes :(
+    characterlist = [
+        'bf',
+        'dad',
+        'gf',
+        'spooky',
+        'pico',
+        'mom',
+        'mom-car',
+        'bf-car',
+        'parents-christmas',
+        'monster-christmas',
+        'bf-christmas',
+        'monster'
+    ]
+    
     with open(song, 'r') as file:
         contents = file.read()
         data = ''.join(a for a in contents if a in string.printable)
@@ -13,13 +30,20 @@ def randomize(song):
 
     data = json.loads(data)
 
+    if character:
+        data['song']['player1'] = random.choice(characterlist)
+        data['song']['player2'] = random.choice(characterlist)
     for a in data['song']['notes']:
+        if order:
+            a['mustHitSection'] = bool(random.getrandbits(1))
         for b in a['sectionNotes']:
             if b[1] > 3:
                 b[1] = random.randint(4, 7)
             else:
                 b[1] = random.randint(0, 3)
     for a in data['notes']:
+        if order:
+            a['mustHitSection'] = bool(random.getrandbits(1))
         for b in a['sectionNotes']:
             if b[1] > 3:
                 b[1] = random.randint(4, 7)
@@ -34,10 +58,12 @@ def randomize(song):
 def main():
     directories = [a for a in os.listdir('./assets/data') if '.' not in a and a != 'smash'] # smash doesn't work :(
 
+    characters = input("Randomize characters? (y/n): ")
+    order = input("Randomize sections to hit? (y/n): ")
     for a in directories:
         for b in os.listdir(f'./assets/data/{a}'):
             print(f"Randomizing {b}")
-            randomize(f'assets/data/{a}/{b}')
+            randomize(f'assets/data/{a}/{b}', characters, order)
             print(f"{b} randomized")
     input("Successfully randomized. Please press ENTER...")
 
