68cbb3f98bae8080d6d1aafa0549f938d69aa1f6
Nate Stutte <ngs33@drexel.edu>
2021-01-23 11:18:49 -0500

    Actually UNCOMMENTED THE CODE WHAT AM I DOING

--- fnf-randomizer.py
+++ fnf-randomizer.py
@@ -1,33 +1,45 @@
+#! /usr/bin/env python
+
 import json
 import string
 import random
 import os
 
-directories = os.listdir('./assets/data')
-print(directories)
-# with open('pico-hard.json', 'r') as file:
-#     contents = file.read()
-#     data = ''.join(a for a in contents if a in string.printable)
-#     end = ''.join(a for a in contents if a not in string.printable)
+def randomize(song):
+    with open(song, 'r') as file:
+        contents = file.read()
+        data = ''.join(a for a in contents if a in string.printable)
+        end = ''.join(a for a in contents if a not in string.printable)
 
-# data = json.loads(data)
+    data = json.loads(data)
 
-# for a in data['song']['notes']:
-#     for b in a['sectionNotes']:
-#         if b[1] > 3:
-#             b[1] = random.randint(4, 7)
-#         else:
-#             b[1] = random.randint(0, 3)
-# for a in data['notes']:
-#     for b in a['sectionNotes']:
-#         if b[1] > 3:
-#             b[1] = random.randint(4, 7)
-#         else:
-#             b[1] = random.randint(0, 3)
+    for a in data['song']['notes']:
+        for b in a['sectionNotes']:
+            if b[1] > 3:
+                b[1] = random.randint(4, 7)
+            else:
+                b[1] = random.randint(0, 3)
+    for a in data['notes']:
+        for b in a['sectionNotes']:
+            if b[1] > 3:
+                b[1] = random.randint(4, 7)
+            else:
+                b[1] = random.randint(0, 3)
 
-# data = json.dumps(data)
+    data = json.dumps(data).replace(' ','')
 
-# data = data.replace(' ','')
+    with open(song, 'w') as file:
+        file.write(data + end)
 
-# with open('pico-hard.json', 'w') as file:
-#     file.write(data + end)
+def main():
+    directories = [a for a in os.listdir('./assets/data') if '.' not in a and a != 'smash'] # smash doesn't work :(
+
+    for a in directories:
+        for b in os.listdir(f'./assets/data/{a}'):
+            print(f"Randomizing {b}")
+            randomize(f'assets/data/{a}/{b}')
+            print(f"{b} randomized")
+    input("Successfully randomized. Please press ENTER...")
+
+if __name__ == "__main__":
+    main()
