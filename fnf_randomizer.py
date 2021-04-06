717f942588a405bd994ffc8d24ff02a54718bb0a
Nate Stutte <ngs33@drexel.edu>
2021-04-05 20:41:56 -0400

    Patched problems with randomizing mod songs

--- fnf_randomizer.py
+++ fnf_randomizer.py
@@ -35,16 +35,17 @@ def randomize(path, song, notes, character, order):
                         b[1] = random.randint(4, 7)
                     else:
                         b[1] = random.randint(0, 3)
-        for a in data['notes']:
-            if order:
-                a['mustHitSection'] = bool(random.getrandbits(1))
-            if notes:
-                a['sectionNotes'] = [list(t) for t in {tuple(item) for item in a['sectionNotes']}]
-                for b in a['sectionNotes']:
-                    if b[1] > 3:
-                        b[1] = random.randint(4, 7)
-                    else:
-                        b[1] = random.randint(0, 3)
+        if 'notes' in data:
+            for a in data['notes']:
+                if order:
+                    a['mustHitSection'] = bool(random.getrandbits(1))
+                if notes:
+                    a['sectionNotes'] = [list(t) for t in {tuple(item) for item in a['sectionNotes']}]
+                    for b in a['sectionNotes']:
+                        if b[1] > 3:
+                            b[1] = random.randint(4, 7)
+                        else:
+                            b[1] = random.randint(0, 3)
 
         data = json.dumps(data).replace(' ','')
 
